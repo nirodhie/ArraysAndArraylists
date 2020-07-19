71f86c7ee09bf83f3851dd77db394cf1feab4048
nirodhie <lteska2@gmail.com>
2020-07-19 08:04:27 +0200

    - basic array iteration with foreach

---
+++ src/com/teska/Main.java
@@ -0,0 +1,15 @@
+package com.teska;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[] simpleArray = {1456,24654,3,4654,5,6,7,8,9};
+
+	for (Integer i : simpleArray)
+         {
+        System.out.println(i);
+    }
+
+
+    }
+}
