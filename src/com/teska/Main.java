d146d4ac66c239ad2ca5897d0fe605f0dd323b65
nirodhie <lteska2@gmail.com>
2020-07-21 15:07:18 +0200

    - wyświetlanie całej array jako metoda - scanner jako metoda - wybór czy oddzielamy wartości scannera ',' czy '.'

--- src/com/teska/Main.java
+++ src/com/teska/Main.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     private static Scanner s = new Scanner(System.in);
-
+    //s.useLocale(Locale.US); //wprowadzanie liczb z kropką jako separatorem
+    //s.useLocale(Locale.GERMANY); //wprowadzanie danych z przecinkiem
     public static void main(String[] args) {
     int[] simpleArray = {1456, 24654, 3, 4654, 534245, 634, 7, 8, 9}; //tak można zainicjować tylko raz
     printArray(simpleArray);
 
 
+    printArray(readUsingScanner(3));
 
 
     }
@@ -22,6 +24,17 @@ public class Main {
         }
     }
 
+    public static int[] readUsingScanner(int input){
+        int[] createdArray = new int [input]; //nowa macierz o podanej przez nas wielkości
+        for (int i : createdArray) //dla wszystkich elementów createdArray
+        {
+            System.out.println("Enter value: ");
+            int value = s.nextInt(); //jeśli user poda jakąś wartość int to ją zapisz do value
+            s.nextLine(); //czekaj aż user coś wpisze i naciśnie enter
+            createdArray[i] = value; // zapisz 'value' do kolejnej pozycji na macierzy
+        }
+        return createdArray; //zwróć całą macierz
+    }
 
     }
 
